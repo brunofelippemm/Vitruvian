1cc10955edfa30b9b42081623b1a92524e1904d5
Pedro Rossi <pedromanuel.rossi@gmail.com>
2019-11-07 20:05:28 -0600

    wtf implementation

--- VitruvianProject/Tastly/__init__.py
+++ VitruvianProject/Tastly/__init__.py
@@ -3,6 +3,9 @@ The flask application package.
 """
 
 from flask import Flask
+from config import Config
+
 application = app = Flask(__name__)
+app.config.from_object(Config)
 
 import Tastly.views
--- VitruvianProject/Tastly/forms.py
+++ VitruvianProject/Tastly/forms.py
@@ -1,14 +1,14 @@
 from wtforms import Form, IntegerField, validators, SubmitField, SelectField
+from flask_wtf import FlaskForm
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length, NumberRange
 
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
-
-class InputForm(Form):
-    age = IntegerField('Age', [validators.DataRequired(message=('Please enter your age'))])
+class InputForm(FlaskForm):
+    age = IntegerField('Age', [validators.DataRequired(message=('Please enter your age')), validators.NumberRange(min=15, max=100, message=('Please enter a valid number'))])
 
     gender = SelectField('Gender',[validators.DataRequired(message=('Please select a gender'))], choices=[('male', 'Male'), ('female', 'Female')])
 
-    musicGenre = SelectField('Music genre', [validators.DataRequired(message=('Please select a music genre'))], choices=[('rock', 'Rock'), ('pop', 'Pop'), ('electronic', 'Electronic'), ('country', 'Country')])
+    #musicGenre = SelectField('Music genre', [validators.DataRequired(message=('Please select a music genre'))], choices=[('rock', 'Rock'), ('pop', 'Pop'), ('electronic', 'Electronic'), ('country', 'Country')])
 
     beverage =  SelectField('Beverage', [validators.DataRequired(message=('Please select a beverage'))], choices=[('beer', 'Beer'), ('wine', 'Wine'), ('wiskey', 'Wiskey'), ('tequila', 'Tequila'), ('vodka', 'Vodka'), ('dosent drink', 'Doesnt drink')])
 
-    smoke = SelectField('Do you somoke?', [validators.DataRequired(message=('Please select a smoking option'))], choices=[('yes', 'Yes'), ('no', 'No')]) 
+    smoke = SelectField('Do you smoke?', [validators.DataRequired(message=('Please select a smoking option'))], choices=[('yes', 'Yes'), ('no', 'No')]) 
